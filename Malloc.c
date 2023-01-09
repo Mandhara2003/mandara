6dae836546d0a34c7c8d78a4f5e463b9d6f4c798
Mandhara2003 <115629360+Mandhara2003@users.noreply.github.com>
2023-01-09 21:17:27 +0530

    Mallic.c

---
+++ Malloc.c
@@ -0,0 +1,18 @@
+#include<studio.h>
+#include<stdlib.h>
+Int main()
+{
+Int i,n,sum=0,*p
+Printf("enter the size");
+Scanf("%d",&n);
+P=(int*)mallic(n*size of(int));
+Printf("enter the elements");
+for(i=0;i<n;i++)
+{
+Scanf("%d",p+i);
+Sum=sum+(p+i);
+}
+Printf("\n sum=%d",sum);
+free(p);
+return 0;
+}
